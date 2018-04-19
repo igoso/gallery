36661a63112020bb25df7cc815f6821596caeedb
igoso <nolyta@qq.com>
2018-04-19 17:49:10 +0800

    update web security and add download test.

---
+++ src/main/java/com/igoso/me/gallery/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.igoso.me.gallery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by igoso on 18-4-19.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/download", "upload", "/error").permitAll()
+                .anyRequest().authenticated();
+    }
+}
