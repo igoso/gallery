4e3b878b94ca7ee832ce50cd2e4cc415cab59467
igoso <nolyta@qq.com>
2018-06-19 16:28:46 +0800

    support scheduling

--- src/main/java/com/igoso/me/gallery/GalleryApplication.java
+++ src/main/java/com/igoso/me/gallery/GalleryApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan(basePackages = "com.igoso.me.gallery.dao")
 @EnableTransactionManagement
+@EnableScheduling
 @SpringBootApplication
 public class GalleryApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(GalleryApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(GalleryApplication.class, args);
+        }
 }
