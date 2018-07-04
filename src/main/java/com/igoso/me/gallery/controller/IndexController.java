20373f955e015a9307adc4de41bae14b0307377c
igoso <nolyta@qq.com>
2018-07-04 22:49:18 +0800

    new home page

--- src/main/java/com/igoso/me/gallery/controller/IndexController.java
+++ src/main/java/com/igoso/me/gallery/controller/IndexController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-//    @RequestMapping("/")
-//    public String index() {
-//        return "index";
-//    }
-
     @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/oss")
     public String upload() {
         return "upload";
     }
